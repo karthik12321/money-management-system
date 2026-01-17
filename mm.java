e5169fc4d2410f825eb9d39433d72b9e53881b66
Karthik Nair <itskarthiknair@gmail.com>
2026-01-18 00:58:18 +0530

    menu created (income,expenxe,view balance feature created)

--- mm.java
+++ mm.java
@@ -1,48 +1,134 @@
 import java.util.*;
+
 public class mm
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
 
-
-    Scanner sc = new Scanner(System.in);
-    //HOw many things are there
-    System.out.println("How many things did you spend money on this month? ");
-    int n = sc.nextInt();
-    sc.nextLine();
-    String[] expenseName = new String[n];
-    double[] expenseAmount = new double[n];
-    for(int i=0;i<n;i++){
-        System.out.println("\nExpense "+(i+1));
-
-
-        System.out.println("Enter expense name: ");
-        expenseName[i] = sc.nextLine();
-
-        System.out.println("Enter expense amount of "+expenseName[i] + ": ");
-        expenseAmount[i] = sc.nextInt();
+        TransactionManager tm = new TransactionManager();
+        while(true)
+        {
+        System.out.println("================================");
+        System.out.println("    Money Management System");
+        System.out.println("================================");
+        System.out.println("  1. Add Income");
+        System.out.println("  2. Add Expense");
+        System.out.println("  3. View balance");
+        System.out.println("  4. Exit\n");
+        System.out.print("Your choice: ");
+        int choice = sc.nextInt();
         sc.nextLine();
-    }
-        double total = 0;
-        System.out.println("\n-----Monthly Expenditure Summary-------");
-        for (int i = 0; i < n; i++) {
-            System.out.println(expenseName[i] + ": "+expenseAmount[i]);
-            total += expenseAmount[i];
+
+        switch(choice)
+        {
+            case 1:
+                {
+                    System.out.print("\nEnter income amount: ");
+                    int amt = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter income type: ");
+                    String type = sc.nextLine();
+                    tm.addincome(amt, type);
+                    System.out.println("Income added successfully!\n");
+                    break;
+                }
+
+            case 2:
+                {
+                    System.out.print("\nEnter expense amount: ");
+                    int amt = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter expense type: ");
+                    String type = sc.nextLine();
+                    tm.addexpense(amt, type);
+                    System.out.println("Expense added successfully!\n");
+                break;
+                }
+                
+            case 3:
+                {
+                    tm.checkbalance();
+                break;
+                }
+            case 4:
+                {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+        
+                break;}
         }
-        System.out.println("----------------------------------");
-        System.out.println("Total Monthly Expenditure "+total);
+}
+    }
+}
 
-        //monthly income
-        System.out.println("Enter your monthly income:");
-        int income = sc.nextInt();
-        income -= total;
-        System.out.println("You have "+income+" to invest or save :");
-        System.out.println("What would you like to with this money : 1 FD : 2 Stocks 3 : Health insurance 4 : Nothing ");
-       int x = sc.nextInt();
-       switch(x){
-           case 1:
-       }
+class Transaction
+{
+    String type;
+    int amount;
+    Transaction(int amt,String type)
+    {
+        this.amount = amt;
+        this.type = type;
+    }
+}
+class TransactionManager
+{
+    Transaction[] income = new Transaction[2];
+    Transaction[] expense = new Transaction[2];
+    int incomecount = 0; 
+    int expensecount = 0;
+    
+    void addincome(int amt,String type)
+    {
+        ensurecapacity();
+        income[incomecount++] = new Transaction(amt, type);
+        
+    }
 
-
-
+    void addexpense(int amt,String type)
+    {
+        ensurecapacity();
+        expense[expensecount++] = new Transaction(amt, type);
+        
+    }
+    void ensurecapacity()
+    {
+        if(incomecount == income.length)
+        {
+            Transaction[] newincarr = new Transaction[income.length * 2];
+            for(int i = 0 ; i <income.length ; i++)
+            {
+                newincarr[i] = income[i];
+            }
+            income = newincarr;
+        }
+        if(expensecount == expense.length)
+        {
+            Transaction[] newexparr = new Transaction[expense.length * 2];
+            for(int i = 0 ; i <expense.length ; i++)
+            {
+                newexparr[i] = expense[i];
+            }
+            expense = newexparr;
+        }
+    }
+    static int balance = 0;
+    //check balance
+    void checkbalance()
+    {
+        int totexpense = 0;
+        int totincome = 0; 
+        for(int i = 0 ; i<incomecount ; i++)
+        {
+            totincome += income[i].amount;
+        }
+        for(int i = 0 ; i<expensecount ; i++)
+        {
+            totexpense += expense[i].amount;
+        }
+        //calculate balance
+        balance = totincome - totexpense;
+        System.out.println("Your balance is: " + balance + "\n");
     }
 }
